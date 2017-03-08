fbea943dc5db84d90b660e1855c85e3220356606
miniminiming <710473214@qq.com>
2017-03-08 12:21:05 +0800

    Initial commit

---
+++ toastlibrary/src/main/java/com/ming/toastlibrary/ToastUtilssssss.java
@@ -0,0 +1,14 @@
+package com.ming.toastlibrary;
+import android.app.Activity;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2017/3/7.
+ */
+
+public class ToastUtilssssss {
+    public static void showToast(Activity activity){
+        Toast.makeText(activity,"我士尼霸霸",Toast.LENGTH_SHORT).show();
+    }
+
+}
